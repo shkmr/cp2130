4e61bcf6d14288ec10b5425af512c6f4ad7971b4
Shigenobu Kimura <skimu@me.com>
2019-07-14 13:09:25 -0500

    fo

--- cp2130.c
+++ cp2130.c
@@ -478,11 +478,71 @@ void cp2130_reset_gpio10(usbcom_t com) {cp2130_set_gpio_values(com,       0, GPI
 #ifdef CP2130_MAIN
 
 #include <unistd.h>
+#include <iconv.h>
+
+void dump(char *name, char *buf, size_t siz)
+{
+  int n;
+  printf("%s siz=%zu\n",  name, siz);
+  for (n=0; n < siz; n++) {
+    printf("%04d: %2d '%c'\n", n, buf[n], buf[n]);
+  }
+}
+
+void utf16_to_utf8(char *utf16, size_t utf16len,
+                   char *utf8,  size_t utf8len)
+{
+  iconv_t conv;
+  conv = iconv_open("UTF-8", "UTF-16LE");
+  iconv(conv, &utf16, &utf16len, &utf8, &utf8len);
+  iconv_close(conv);
+}
+
+void print_dev_info(usbcom_t com)
+{
+  enum {
+        BUFLEN = 256
+  };
+  char utf16[BUFLEN];
+  char utf8[BUFLEN];
+  int major, minor;
+
+  cp2130_get_readonly_version(com, &major, &minor);
+  printf("Connected. cp2130 version %d.%d\n", major, minor);
+
+  memset(utf16, 0, BUFLEN);
+  memset(utf8,  0, BUFLEN);
+  cp2130otp_get_manufacturing_string(com, utf16, BUFLEN);
+  utf16_to_utf8(utf16, BUFLEN, utf8, BUFLEN);
+  /* dump("utf16", utf16, BUFLEN);
+     dump("utf8",  utf8,  BUFLEN); */
+  printf("manufacturer: \"%s\"\n", utf8);
+
+  memset(utf16, 0, BUFLEN);
+  memset(utf8,  0, BUFLEN);
+  cp2130otp_get_product_string(com, utf16, BUFLEN);
+  utf16_to_utf8(utf16, BUFLEN, utf8, BUFLEN);
+  printf("product: \"%s\"\n", utf8);
+
+  memset(utf16, 0, BUFLEN);
+  memset(utf8,  0, BUFLEN);
+  cp2130otp_get_serial_string(com, utf16, BUFLEN);
+  utf16_to_utf8(utf16, BUFLEN, utf8, BUFLEN);
+  printf("serial: \"%s\"\n", utf8);
+
+  cp2130otp_get_usb_config(com, utf8);
+  printf("USB device release number: %d.%d\n", utf8[6], utf8[7]);
+  if (utf8[8] == 1)
+    printf("USB high priority: write\n");
+  else if (utf8[8] == 0)
+    printf("USB high priority: read\n");
+  else
+    printf("USB high priority: error\n");
+}
 
 int main(int c, char *v[])
 {
   int vendor, product, n;
-  int major, minor;
   usbcom_t com;
 
   printf("cp2130 main\n");
@@ -494,10 +554,9 @@ int main(int c, char *v[])
   if (com == NULL)
     err(1, "cp2130: open failed");
 
-  cp2130_get_readonly_version(com, &major, &minor);
-  printf("connected version %d.%d\n", major, minor);
+  print_dev_info(com);
 
-  for (n=0; n < 10; n++) {
+  for (n=0; n < 2; n++) {
     printf("LED ON:  Event Counter=%4d\n", cp2130_get_event_counter_count(com));
     cp2130_reset_gpio07(com);
     sleep(1);
