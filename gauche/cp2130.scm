6cb895d6732a3fbb6e97ffc57df648f47036697f
Shigenobu Kimura <skimu@me.com>
2019-07-22 21:15:49 -0500

    add some convenience functions.

--- gauche/cp2130.scm
+++ gauche/cp2130.scm
@@ -2,16 +2,31 @@
 ;;; cp2130
 ;;;
 
-(define-module cp2130 (export-all))
+(define-module cp2130
+  (use gauche.uvector)
+  (use gauche.charconv)
+  (export-all))
 
 (select-module cp2130)
 
 ;; Loads extension
 (dynamic-load "cp2130")
 
-;;
-;; Put your Scheme definitions here
-;;
+(define (cp2130otp-get-manufacturing-string dev)
+  (let ((buf (make-u8vector 128 0)))
+    (cp2130otp-get-manufacturing-string& dev buf)
+    (ces-convert buf 'utf-16le)))
 
+(define (cp2130otp-get-product-string dev)
+  (let ((buf (make-u8vector 128 0)))
+    (cp2130otp-get-product-string& dev buf)
+    (ces-convert buf 'utf-16le)))
+
+(define (cp2130otp-get-serial-string dev)
+  (let ((buf (make-u8vector 128 0)))
+    (cp2130otp-get-serial-string& dev buf)
+    (ces-convert buf 'utf-16le)))
+
+;; EOF
 
 
